20295f84353afec4e47fcfee18ae0a7544c18847
Matthew <52621525+H11Maitree@users.noreply.github.com>
2023-06-03 00:33:24 +0700

    Initial commit

---
+++ assets/js/cityu.config.js
@@ -0,0 +1,17 @@
+/*
+ *
+ * City University of Hong Kong
+ * ----------------------------
+ * Site Config
+ *
+ */
+
+'use strict';
+
+var cityuSiteConfig = {
+  mode: 'auto', // auto|prod|dev
+  aos: 'cityu', // cityu|base|none
+  logoutLink: 'on', // on|off
+  cookieNotice: 'on', // on|off
+  XXLBreakpoint: 'on', // on|off
+};
